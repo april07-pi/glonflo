34b2530ea3ae2f08256aa4a7a89327f0908a3642
Nokwazi Nobuhle Xaba <xabanokwazi008@gmail.com>
2025-11-18 21:28:59 +0200

    Add About section to home.html

--- home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Welcome to Glo N Flo. Period Care is Self-Care!');
-});
-
-module.exports = router;
